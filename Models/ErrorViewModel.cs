b77ed223e48319c55e7d322374e7dae309f92081
Maiara Evangelista <maiaraevangelista933@gmail.com>
2021-01-12 12:51:00 -0300

    treinando

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Aspnet_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
